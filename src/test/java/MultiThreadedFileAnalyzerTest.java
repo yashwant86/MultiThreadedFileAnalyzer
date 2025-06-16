2040855a505221c18ba6b176eed31070f0953b9b
yashwant86 <yashwant8631440@gmail.com>
2025-06-16 20:10:36 +0200

    new dependencies and test added

--- src/test/java/MultiThreadedFileAnalyzerTest.java
+++ src/test/java/MultiThreadedFileAnalyzerTest.java
@@ -36,4 +36,52 @@ public class MultiThreadedFileAnalyzerTest {
 		assertEquals(0, result.getKeywordOccurrences());
 		assertEquals("", result.getLongestWord());
 	}
+
+	@Test
+	void testAnalyzeFile_punctuationAndSpaces() throws IOException {
+		File tempFile = File.createTempFile("punctuation", ".txt");
+		tempFile.deleteOnExit();
+
+		try (FileWriter writer = new FileWriter(tempFile)) {
+			writer.write("hello,  world!!   keyword keyword!  ");
+		}
+
+		FileAnalysisResult result = MultiThreadedFileAnalyzer.analyzeFile(tempFile, "keyword");
+
+		assertEquals(4, result.getWordCount());
+		assertEquals(1, result.getKeywordOccurrences());
+		assertEquals("keyword!", result.getLongestWord());
+	}
+
+	@Test
+	void testAnalyzeFile_caseInsensitiveKeyword() throws IOException {
+		File tempFile = File.createTempFile("caseInsensitive", ".txt");
+		tempFile.deleteOnExit();
+
+		try (FileWriter writer = new FileWriter(tempFile)) {
+			writer.write("Keyword keyword KEYWORD Keyword");
+		}
+
+		FileAnalysisResult result = MultiThreadedFileAnalyzer.analyzeFile(tempFile, "keyword");
+
+		assertEquals(4, result.getWordCount());
+		assertEquals(4, result.getKeywordOccurrences());
+		assertEquals("Keyword", result.getLongestWord());
+	}
+
+	@Test
+	void testAnalyzeFile_leadingTrailingWhitespace() throws IOException {
+		File tempFile = File.createTempFile("whitespace", ".txt");
+		tempFile.deleteOnExit();
+
+		try (FileWriter writer = new FileWriter(tempFile)) {
+			writer.write("   keyword  test \n  another  line   ");
+		}
+
+		FileAnalysisResult result = MultiThreadedFileAnalyzer.analyzeFile(tempFile, "keyword");
+
+		assertEquals(6, result.getWordCount());
+		assertEquals(1, result.getKeywordOccurrences());
+		assertEquals("keyword", result.getLongestWord());
+	}
 }

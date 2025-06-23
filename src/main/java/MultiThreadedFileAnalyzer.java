baa7d18228717ca1f1c824d76a30c915d620ec8e
yashwant86 <yashwant8631440@gmail.com>
2025-06-23 11:01:28 +0200

    new dependencies and test added

--- src/main/java/MultiThreadedFileAnalyzer.java
+++ src/main/java/MultiThreadedFileAnalyzer.java
@@ -63,6 +63,11 @@ public class MultiThreadedFileAnalyzer {
 			String line;
 			while ((line = reader.readLine()) != null) {
 				String[] words = line.split("\\s+");
+				for (String word: words){
+					if(word.trim().isEmpty()){
+						continue; // Skip empty lines
+					}
+				}
 				wordCount += words.length;
 
 				for (String word : words) {
